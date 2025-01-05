ae21a150872976db73c183eb6f6b45cd578f7690
MuhamadSahrul <60306963+muhamadsahrulll@users.noreply.github.com>
2025-01-05 20:31:09 +0700

    belum fix

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -6,8 +6,8 @@ using UnityEngine.SceneManagement;
 public class DialogManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] dialogObjects; // Array untuk GameObject dialog
+    [SerializeField] private AudioClip[] dialogAudioClips; // Array untuk AudioClip dialog
     [SerializeField] private string nextSceneName;       // Nama scene selanjutnya
-    //SceneController sceneController;
 
     private int currentDialogIndex = 0;                 // Indeks dialog yang sedang ditampilkan
 
@@ -17,6 +17,12 @@ public class DialogManager : MonoBehaviour
         for (int i = 0; i < dialogObjects.Length; i++)
         {
             dialogObjects[i].SetActive(i == 0);
+        }
+
+        // Mainkan suara untuk dialog pertama jika ada
+        if (dialogAudioClips.Length > 0 && dialogAudioClips[0] != null)
+        {
+            MusicManager.instance.PlaySFX(dialogAudioClips[0]);
         }
     }
 
@@ -29,7 +35,7 @@ public class DialogManager : MonoBehaviour
         }
     }
 
-    private void ShowNextDialog()
+    public void ShowNextDialog()
     {
         // Nonaktifkan dialog yang sedang aktif
         if (currentDialogIndex < dialogObjects.Length)
@@ -40,16 +46,21 @@ public class DialogManager : MonoBehaviour
         // Pindah ke dialog berikutnya
         currentDialogIndex++;
 
-        // Jika masih ada dialog, aktifkan dialog berikutnya
+        // Jika masih ada dialog, aktifkan dialog berikutnya dan mainkan suara
         if (currentDialogIndex < dialogObjects.Length)
         {
             dialogObjects[currentDialogIndex].SetActive(true);
+
+            // Mainkan suara untuk dialog yang baru aktif
+            if (currentDialogIndex < dialogAudioClips.Length && dialogAudioClips[currentDialogIndex] != null)
+            {
+                MusicManager.instance.PlaySFX(dialogAudioClips[currentDialogIndex]);
+            }
         }
         else
         {
             // Jika sudah tidak ada dialog, pindah ke scene berikutnya
             SceneManager.LoadScene(nextSceneName);
-            //sceneController.NextLevel("level1");
         }
     }
 }
